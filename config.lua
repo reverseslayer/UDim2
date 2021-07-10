de6c6a2038c7528ed546bdc241535b943caa4364
reverseslayer <dholloway543@gmail.com>
2021-07-09 18:51:18 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -7,6 +7,13 @@ application =
 {
 	content =
 	{
+		--[[
+			In order for UDim2 to work properly we need to remove
+				width = 1080,
+				height = 1920,
+				scale = "letterbox",
+			so that corona sdk / solar2d use the phones real screen resolution no scaling
+		]]
 		antialias = true,
 		fps = 60,
 		
